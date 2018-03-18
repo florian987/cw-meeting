e8ef87c1169cc5119ac04cea28f30dbc4d9120a4
Florian Blond <florian.blond@gmail.com>
2018-03-18 13:16:21 +0100

    add admin list
    add confirm payment

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -7,6 +7,7 @@ use App\Form\SubscriptionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubscriptionController extends Controller
@@ -69,13 +70,13 @@ class SubscriptionController extends Controller
         return $this->render(
             'list.html.twig',
             [
-                'attendees' => $attendees
+                'attendees' => $attendees,
             ]
         );
     }
 
     /**
-     * @Route("/listraw")
+     * @Route("/list/iframe")
      * @param EntityManagerInterface $em
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -84,11 +85,61 @@ class SubscriptionController extends Controller
         $attendees = $em->getRepository('App:Attendee')->findBy([], ['updatedAt' => 'desc']);
 
         return $this->render(
-            'listraw.html.twig',
+            'list_raw.html.twig',
             [
-                'attendees' => $attendees
+                'attendees' => $attendees,
             ]
         );
+    }
+
+    /**
+     * @Route("/admin/attendees")
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function adminListAction(EntityManagerInterface $em)
+    {
+        $attendees = $em->getRepository('App:Attendee')->findBy([], ['createdAt' => 'asc']);
+
+        return $this->render(
+            'list_admin.html.twig',
+            [
+                'attendees' => $attendees,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/admin/attendees/{id}/confirm")
+     * @param Attendee $attendee
+     * @param \Swift_Mailer $mailer
+     * @return Response
+     */
+    public function confirmPaymentAction(Attendee $attendee, \Swift_Mailer $mailer, EntityManagerInterface $em)
+    {
+        if ($attendee->getHasPaid()) {
+            return new Response('Already paid');
+        }
+
+        $message = (new \Swift_Message('Inscription Meeting au Pal - 21 avril 2018'))
+            ->setFrom('benjamin@coastersworld.fr')
+            ->setTo($attendee->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'emails/confirm_payment.txt.twig'
+                ),
+                'text/plain'
+            );
+
+        $mailer->send($message);
+
+        $attendee->setHasPaid(1);
+        $em->persist($attendee);
+        $em->flush();
+
+        $name = $attendee->getFirstname().' '.$attendee->getName();
+
+        return new Response("Paiment pour $name validé");
     }
 
     private function sendPaymentEmail(Attendee $attendee, \Swift_Mailer $mailer)
